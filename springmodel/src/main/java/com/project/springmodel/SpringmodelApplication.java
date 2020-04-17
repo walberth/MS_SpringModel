660317dd066af112f9c14388a1da115ea39deae4
Walberth <w.felipe.gutierrez@gmail.com>
2020-04-17 11:20:12 -0500

    creación del proyecto

---
+++ springmodel/src/main/java/com/project/springmodel/SpringmodelApplication.java
@@ -0,0 +1,13 @@
+package com.project.springmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmodelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmodelApplication.class, args);
+    }
+
+}
---
+++ springmodel/src/test/java/com/project/springmodel/SpringmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmodelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
